f2060dd9175e78d679d774090ba7e16bd4dcd883
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2015-02-03 18:32:12 +0100

    make it work

--- src/ClangASTBear.py
+++ src/ClangASTBear.py
@@ -1,6 +1,9 @@
 from coalib.bears.LocalBear import LocalBear
+import clang.cindex
 
 
 class ClangASTBear(LocalBear):
     def run_bear(self, filename, file, *args):
-        self.debug_msg(filename)
+        index = clang.cindex.Index.create()
+        tree = index.parse(filename)
+        self.debug_msg(str(tree.spelling))
