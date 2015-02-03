0ad648c501d6abf9b0794506561c26bb810d8fcb
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2015-02-03 18:54:33 +0100

    print AST

--- samples/sample.c
+++ samples/sample.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+//#include <stdio.h>
 
 int main(void) {
     for(int i; i < 5; i++) {
--- src/ClangASTBear.py
+++ src/ClangASTBear.py
@@ -3,7 +3,23 @@ import clang.cindex
 
 
 class ClangASTBear(LocalBear):
+    def print_clang_cursor(self, cursor, indent=""):
+        if cursor is None:
+            return
+        if len(indent) > 40:
+            print("ABORTING")
+            return
+
+        self.debug_msg(indent + "Got child:")
+        self.debug_msg(indent + "KIND:", cursor.kind)
+        self.debug_msg(indent + "LOC :", cursor.location)
+
+        for child in cursor.get_children():
+            self.print_clang_cursor(child, indent+"| ")
+
     def run_bear(self, filename, file, *args):
         index = clang.cindex.Index.create()
         tree = index.parse(filename)
         self.debug_msg(str(tree.spelling))
+
+        self.print_clang_cursor(tree.cursor)
