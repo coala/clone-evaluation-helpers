673bc0a9913f92e05ac3a878e7b11d2b8efd0f5e
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2015-04-12 18:20:29 +0200

    print only cursors from current file

--- src/ClangASTBear.py
+++ src/ClangASTBear.py
@@ -3,23 +3,26 @@ import clang.cindex
 
 
 class ClangASTBear(LocalBear):
-    def print_clang_cursor(self, cursor, indent=""):
+    def print_clang_cursor(self, cursor, filename, indent=""):
         if cursor is None:
             return
         if len(indent) > 40:
             print("ABORTING")
             return
 
-        self.debug_msg(indent + "Got child:")
-        self.debug_msg(indent + "KIND:", str(cursor.kind))
-        self.debug_msg(indent + "LOC :", str(cursor.location))
+        file = cursor.location.file
+        name = "(No location)" if file is None else file.name.decode()
+
+        if str(name) == str(filename):
+            self.debug_msg(indent + "Got child:")
+            self.debug_msg(indent + "KIND:", str(cursor.kind))
+            self.debug_msg(indent + "FILE:", str(name))
 
         for child in cursor.get_children():
-            self.print_clang_cursor(child, indent+"| ")
+            self.print_clang_cursor(child, filename, indent+"| ")
 
     def run(self, filename, file, *args):
         index = clang.cindex.Index.create()
         tree = index.parse(filename)
-        self.debug_msg(str(tree.spelling))
 
-        self.print_clang_cursor(tree.cursor)
+        self.print_clang_cursor(tree.cursor, filename)
