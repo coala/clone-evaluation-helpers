5ad1cbc96eb2ebc4cf1a2e6b41f439562ea67bbd
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2015-04-16 12:15:20 +0200

    add param to sampel

--- samples/sample.c
+++ samples/sample.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-#include "../not_existant.c"
+#include "not_existant.c"
 
 int test(void);
 
-int main(void) {
-    smile();
+int main(int t) {
+    smile(t);
     for(int i; i < 5; i++) {
         printf("i is %d", i);
     }
