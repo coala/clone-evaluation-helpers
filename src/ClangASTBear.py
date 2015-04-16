88bac2655fe093bcad97a0c8ee8be24b043f884f
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2015-04-16 11:40:44 +0200

    add get_variables, add scope

--- src/ClangASTBear.py
+++ src/ClangASTBear.py
@@ -1,37 +1,41 @@
 from coalib.bears.LocalBear import LocalBear
 import clang.cindex as ci
 
-
 class ClangASTBear(LocalBear):
     @staticmethod
     def is_function_declaration(cursor):
         return cursor.kind == ci.CursorKind.FUNCTION_DECL
 
-    def get_vectors(self,
-                    cursor,
-                    filename,
-                    stack=None):
+    def get_variables(self,
+                   cursor,
+                   stack=None):
         if stack is None:
             stack = []
+
+        self.debug(len(stack)*"|", "Got child:")
+        self.debug(len(stack)*"|", "SCOP:", str(stack.count(ci.CursorKind.COMPOUND_STMT)))
+        self.debug(len(stack)*"|", "KIND:", str(cursor.kind))
+        self.debug(len(stack)*"|", "USR :", str(cursor.get_usr()))
+        self.debug(len(stack)*"|", "DISP:", str(cursor.displayname))
+        if cursor.is_definition():
+            self.debug(len(stack)*"|", "DEFI:", str(cursor.get_definition()))
+
+        stack.append(cursor.kind)
+        for child in cursor.get_children():
+            self.get_variables(child, stack)
+        stack.pop()
+
+    def get_vectors(self,
+                    cursor,
+                    filename):
         file = cursor.location.file
         name = None if file is None else file.name.decode()
 
-        if str(name) == str(filename):
-            if self.is_function_declaration(cursor):
-                self.warn("THIS IS A FUNCTION DECL")
-            self.debug(len(stack)*"|", "Got child:")
-            self.debug(len(stack)*"|", "STACK:", *stack)
-            self.debug(len(stack)*"|", "KIND:", str(cursor.kind))
-            self.debug(len(stack)*"|", "FILE:", str(name))
-            self.debug(len(stack)*"|", "USR :", str(cursor.get_usr()))
-            self.debug(len(stack)*"|", "DISP:", str(cursor.displayname))
-            if cursor.is_definition():
-                self.debug(len(stack)*"|", "DEFI:", str(cursor.get_definition()))
+        if str(name) == str(filename) and self.is_function_declaration(cursor):
+            self.get_variables(cursor)
 
-        stack.append(str(cursor.kind))
         for child in cursor.get_children():
-            self.get_vectors(child, filename, stack=stack)
-        stack.pop()
+            self.get_vectors(child, filename)
 
     def run(self, filename, file, *args):
         index = ci.Index.create()
