c46272aa3140918f15c2c701a3b191114a744e80
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2015-04-16 11:14:00 +0200

    ClangASTBear: Track API change

--- src/ClangASTBear.py
+++ src/ClangASTBear.py
@@ -13,14 +13,14 @@ class ClangASTBear(LocalBear):
         name = None if file is None else file.name.decode()
 
         if str(name) == str(filename):
-            self.debug_msg("Got child:")
-            self.debug_msg("STACK:", *stack, delimiter="\n")
-            self.debug_msg("KIND:", str(cursor.kind))
-            self.debug_msg("FILE:", str(name))
-            self.debug_msg("USR :", str(cursor.get_usr()))
-            self.debug_msg("DISP:", str(cursor.displayname))
+            self.debug("Got child:")
+            self.debug("STACK:", *stack, delimiter="\n")
+            self.debug("KIND:", str(cursor.kind))
+            self.debug("FILE:", str(name))
+            self.debug("USR :", str(cursor.get_usr()))
+            self.debug("DISP:", str(cursor.displayname))
             if cursor.is_definition():
-                self.debug_msg("DEFI:", str(cursor.get_definition()))
+                self.debug("DEFI:", str(cursor.get_definition()))
 
         stack.append(str(cursor.kind))
         for child in cursor.get_children():
