bfa5f14e1f6793eb7de1bf679b1f16dfb833af1f
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2015-04-23 11:22:06 +0200

    sample: Add condition

--- samples/sample.c
+++ samples/sample.c
@@ -8,8 +8,12 @@ int g;
 int main(int t, char* args) {
     smile(t, g);
     t = g;
-    int *asd;
-    t = args[g];
+
+    if (t == g) {
+        int *asd;
+        t = args[g];
+    }
+
     for(int i; i < 5; i++) {
         printf("i is %d", i);
     }
