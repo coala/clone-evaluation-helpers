bf98373e53a007f39ccef4ccfd0e451a48640875
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2015-04-28 17:12:10 +0200

    simplify

--- src/CountVectorGraphMatcher.py
+++ src/CountVectorGraphMatcher.py
@@ -13,11 +13,14 @@ def find_max_match(cm1, cm2):
     cost_matrix = []
 
     for cv1 in cm1:
-        nxt = []
-        cost_matrix.append(nxt)
-        for cv2 in cm2:
-            nxt.append(similarity(cv1, cv2))
+        cost_matrix.append([similarity(cv1, cv2) for cv2 in cm2])
 
-    matched = Munkres().compute(cost_matrix)
+    munkres = Munkres()
+    # Pad manually, we'll access the zeros when iterating over matching
+    cost_matrix = munkres.pad_matrix(cost_matrix)
+    matching = munkres.compute(cost_matrix)
+
+    cost_sum = sum(cost_matrix[x][y] for x, y in matching)
+
     # TODO: Create similarity value for the functions
     raise NotImplementedError
