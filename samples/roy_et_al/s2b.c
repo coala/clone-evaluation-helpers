834685f7a4505b93d2a6ccba60afd6a3cc736db4
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2015-04-28 13:22:33 +0200

    samples: Add clones from paper

---
+++ samples/roy_et_al/s2b.c
@@ -0,0 +1,7 @@
+void sumProd(int n) {
+float s=0.0; //C1
+float p=1.0;
+for (int j=1; j<=n; j++)
+    {s=s + j;
+    p = p * j;
+    foo(p, s); }}
---
+++ samples/roy_et_al/s2c.c
@@ -0,0 +1,7 @@
+void sumProd(int n) {
+int sum=0; //C1
+int prod=1;
+for (int i=1; i<=n; i++)
+    {sum=sum + i;
+    prod = prod * i;
+    foo(sum, prod); }}
