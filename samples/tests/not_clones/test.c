b2a42d33b55b15128e86adccaf7cf47ecaf37c60
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2015-05-20 12:22:44 +0200

    add clone detection benchmarking application

--- samples/tests/not_clones/test.c
+++ samples/tests/not_clones/test.c
@@ -20,6 +20,10 @@ int add(int x, int y) {
     return x + y;
 }
 
+int divide(int x, int y) {
+    return x / y;
+}
+
 void *memcpy(void *restrict dst, const void *restrict src, int len) {
 	char *dt = (char*)dst;
 	char *sc = (char*)src;
@@ -49,4 +53,48 @@ int strlen(const char * s) {
 
 int main(void) {
     return faculty(power(5, 8));
+}
+
+/*
+ * Dumb function that retrieves the minimum of 0 and a given value. However if
+ * a is actually zero the creative writer of this function wants the value of
+ * a mysterious second parameter to be the returned one in the hope it may
+ * be 42!
+ */
+int min_0_a(int a, int b) {
+    if (a != 0) {
+        if (a > 0)
+            return a;
+    } else {
+        if (a != 0)
+            return b;
+    }
+}
+
+/*
+ * Dumb function that is badly written and returns a second parameter if the
+ * first one is smaller than 0, the first one otherwise.
+ */
+int retval(int a, int b) {
+    if (a < 0 && a != 0) {
+        return b;
+    } else {
+        return a;
+    }
+}
+
+struct test_struct {
+    int first;
+}
+
+void do_something(struct test_struct *a) {
+    do_something(a);
+    do_something(a);
+}
+
+void initialize(struct test_struct *a) {
+    a->first = 2;
+    a->second = 2;
+
+    do_something(a);
 }
---
+++ src/CloneDetectionBenchmarkBear.py
@@ -0,0 +1,52 @@
+import re
+from matplotlib import pyplot as plt
+import numpy as np
+
+from coalib.bears.GlobalBear import GlobalBear
+from bears.codeclone_detection.ClangSimilarityBear import ClangSimilarityBear
+
+
+class CloneDetectionBenchmarkBear(GlobalBear):
+    def run(self,
+            dependency_results: dict,
+            clones: str=".*\/clones.*"):
+        '''
+        Checks the quality of the code clone detection on the given test files.
+
+        :param clones:     A regex matching paths of files that contain known
+                           clones. All others are considered non-clones.
+        '''
+        differences = dependency_results["ClangSimilarityBear"][0].contents
+
+        clones_diffs = []
+        non_clones_diffs = []
+
+        for function_1, function_2, difference in differences:
+            if function_1[0] != function_2[0]:
+                continue
+
+            if re.match(clones, function_1[0]) is not None:
+                clones_diffs.append(difference)
+            else:
+                non_clones_diffs.append(difference)
+
+        self.warn(" CD:", str(clones_diffs))
+        self.warn("NCD:", str(non_clones_diffs))
+
+        self.warn("Maximum clone difference:", str(max(clones_diffs)))
+        self.warn("Minimum non-clone difference:", str(min(non_clones_diffs)))
+
+        plt.hlines(1, 0, 1)
+        plt.xlim(0, 1)
+        plt.ylim(0.5, 1.5)
+
+        y = np.ones(np.shape(clones_diffs))
+        plt.plot(clones_diffs, y, '|', ms=40)
+        y = np.ones(np.shape(non_clones_diffs))
+        plt.plot(non_clones_diffs, y, '|', ms=40, color='m')
+        plt.axis('off')
+        plt.show()
+
+    @staticmethod
+    def get_dependencies():
+        return [ClangSimilarityBear]
