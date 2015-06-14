c19688ec721358d2609b26374894137751ba8aa7
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2015-06-14 15:12:23 +0200

    few fixes

--- TweakConditions.py
+++ TweakConditions.py
@@ -14,25 +14,26 @@ from coalib.settings.Section import Section
 from coalib.settings.Setting import Setting
 
 condition_dict = counting_condition_dict(StringConverter(
-    "returned,"
-    "is_condition,"
-    "in_condition,"
-    "in_second_level_condition,"
-    "in_third_level_condition,"
-    "is_assignee,"
-    "is_assigner,"
-    "loop_content,"
-    "second_level_loop_content,"
-    "third_level_loop_content,"
-    "is_param,"
-    "in_sum,"
-    "in_product,"
-    "in_binary_operation,"
-    "member_accessed,"
-    "is_called,"
-    "is_call_param"))
+    """
+used: 0.0,
+third_level_loop_content: 1.0,
+second_level_loop_content: 1.0,
+returned: 0.0,
+member_accessed: 1.0,
+loop_content: 1.2,
+is_param: 1.0,
+is_condition: 0.0,
+is_called: 1.0,
+is_call_param: 1.0,
+is_assigner: 0.0,
+is_assignee: 1.0,
+in_third_level_condition: 1.0,
+in_sum: 1.0,
+in_second_level_condition: 1.0,
+in_product: 1.0,
+in_condition: 1.0,
+in_binary_operation: 1.0"""))
 
-#weighting_range = [1, 0, 0.2, 0.4, 0.6, 0.8, 1.2, 1.4, 1.6, 1.8, 2]
 weighting_range = [1, 0.8, 0.6, 0.4, 0.2, 0, 1.2, 1.4, 1.6, 1.8, 2]
 
 section = Section("default")
@@ -70,7 +71,7 @@ def fitness(c_dict):
 
 
 def optimize_key(c_dict, key, old_fitness):
-    best = (1, old_fitness)
+    best = (c_dict[key], old_fitness)
     for weighting in weighting_range:
         print("Checking", key.__name__, "with", weighting, "...")
         c_dict[key] = weighting
@@ -84,7 +85,7 @@ def optimize_key(c_dict, key, old_fitness):
 
 
 if __name__ == '__main__':
-    new_fitness = 0
+    new_fitness = fitness(sorted_dict)
     for key in sorted_dict.keys():
         new_fitness = optimize_key(sorted_dict, key, new_fitness)
         print("Weightings are:")
