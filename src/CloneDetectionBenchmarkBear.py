df5dac92e694cfddea882a7d81d57bfb93005d1b
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2015-07-01 15:44:15 +0200

    benchmark: fail on wrong ignore

--- src/CloneDetectionBenchmarkBear.py
+++ src/CloneDetectionBenchmarkBear.py
@@ -22,6 +22,7 @@ class CloneDetectionBenchmarkBear(GlobalBear):
         non_clones_diffs = [1]
 
         seen_functions = []
+        must_have = list(self.file_dict.keys())
         for function_1, function_2, difference in differences:
             if function_1[0] not in seen_functions:
                 seen_functions.append(function_1[0])
@@ -31,19 +32,28 @@ class CloneDetectionBenchmarkBear(GlobalBear):
             if function_1[0] != function_2[0]:
                 continue
 
+            if function_1[0] in must_have:
+                must_have.remove(function_1[0])
+
             if re.match(clones, function_1[0]) is not None:
                 clones_diffs.append(difference)
             else:
                 non_clones_diffs.append(difference)
 
+        for filename in must_have:
+            if not re.match(clones, filename):
+                must_have.remove(filename)
+
         print("\n".join(seen_functions))
 
         self.warn(" CD:", str(sorted(clones_diffs)))
         self.warn("NCD:", str(sorted(non_clones_diffs)))
+        if must_have:
+            self.err("The following clone samples were ignored:", must_have)
 
         self.warn("Maximum clone difference:", str(max(clones_diffs)))
         self.warn("Minimum non-clone difference:", str(min(non_clones_diffs)))
-        if max(clones_diffs) > min(non_clones_diffs):
+        if max(clones_diffs) > min(non_clones_diffs) or must_have:
             self.err("Code clone detection failed!")
 
         plt.xlim(0, 1)
