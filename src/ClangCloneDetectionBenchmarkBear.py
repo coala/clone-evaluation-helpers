44ae9ddb2cffb94622ef3727c2186e03a93a7fb0
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2015-07-10 14:18:45 +0200

    Add plot param

--- src/ClangCloneDetectionBenchmarkBear.py
+++ src/ClangCloneDetectionBenchmarkBear.py
@@ -9,12 +9,14 @@ from bears.codeclone_detection.ClangFunctionDifferenceBear import ClangFunctionD
 class ClangCloneDetectionBenchmarkBear(GlobalBear):
     def run(self,
             dependency_results: dict,
-            clones: str=".*\/clones.*"):
+            clones: str=".*\/clones.*",
+            plot: bool=False):
         '''
         Checks the quality of the code clone detection on the given test files.
 
         :param clones:     A regex matching paths of files that contain known
                            clones. All others are considered non-clones.
+        :param plot:       Wether or not to plot the clone/non-clone graph.
         '''
         differences = dependency_results["ClangFunctionDifferenceBear"][0].contents
 
@@ -57,16 +59,17 @@ class ClangCloneDetectionBenchmarkBear(GlobalBear):
         if max(clones_diffs) > min(non_clones_diffs) or must_have:
             self.err("Code clone detection failed!")
 
-        plt.xlim(0, 1)
-        plt.ylim(0.5, 1.5)
+        if plot:
+            plt.xlim(0, 1)
+            plt.ylim(0.5, 1.5)
 
-        y = np.ones(np.shape(clones_diffs))
-        plt.plot(clones_diffs, y, '|', ms=40, color='r', label="Clones")
-        y = np.ones(np.shape(non_clones_diffs))
-        plt.plot(non_clones_diffs, y, '|', ms=40, color='g',
-                 label="Non-Clones")
-        plt.grid()
-        plt.show()
+            y = np.ones(np.shape(clones_diffs))
+            plt.plot(clones_diffs, y, '|', ms=40, color='r', label="Clones")
+            y = np.ones(np.shape(non_clones_diffs))
+            plt.plot(non_clones_diffs, y, '|', ms=40, color='g',
+                     label="Non-Clones")
+            plt.grid()
+            plt.show()
 
     @staticmethod
     def get_dependencies():
